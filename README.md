397396ba786b8e78284ea725bbaf50430f3a8f73
yuvadany <toyuvaraj@gmail.com>
2025-04-22 23:04:21 +0200

    README file updated. PostgreSql properties and Dependencies added with devices Entity

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+# Spring Boot 3 Project
 This is a Spring Boot Project for adding and managing Devices
+
+## 🔧 Tech Stack
+- Java 21
+- Spring Boot 3.4.4
+- PostgreSQL
+- Spring Data JPA
+- Spring Web
+- Swagger/OpenAPI (springdoc-openapi)
+- Maven 3.9.9
---
+++ src/main/java/com/project/device/model/Device.java
@@ -0,0 +1,73 @@
+package com.project.device.model;
+
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "devices")
+public class Device {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String brand;
+
+    @Enumerated(EnumType.STRING)
+    private State state;
+
+    @Column(name = "creation_time", updatable = false)
+    private LocalDateTime creationTime;
+
+    @PrePersist
+    protected void onCreate() {
+        creationTime = LocalDateTime.now();
+    }
+
+    // Getter & Setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return creationTime;
+    }
+
+    public enum State {
+        AVAILABLE,
+        IN_USE,
+        INACTIVE
+    }
+}
