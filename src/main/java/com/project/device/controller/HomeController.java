e3bccee66d6ffa160347420095a33c435f21dd28
yuvadany <toyuvaraj@gmail.com>
2025-04-23 22:08:48 +0200

    Unused imports removed

--- src/main/java/com/project/device/controller/HomeController.java
+++ src/main/java/com/project/device/controller/HomeController.java
@@ -1,19 +1,13 @@
 package com.project.device.controller;
 
 import com.project.device.model.Device;
-import com.project.device.model.ErrorResponse;
 import com.project.device.service.DeviceService;
 import com.project.device.util.DeviceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/devices")
