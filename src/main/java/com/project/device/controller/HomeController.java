3044365fb42b30e29c7d27508686b8d8fb79c7ab
yuvadany <toyuvaraj@gmail.com>
2025-04-23 22:41:54 +0200

    File formatted

--- src/main/java/com/project/device/controller/HomeController.java
+++ src/main/java/com/project/device/controller/HomeController.java
@@ -21,27 +21,27 @@ public class HomeController {
     private DeviceService deviceService;
 
     @PostMapping("/addDevice")
-    public ResponseEntity<Device>  addDevice(@RequestBody Device device) {
+    public ResponseEntity<Device> addDevice(@RequestBody Device device) {
         try {
             Device savedDevice = deviceService.addDevice(device);
             return ResponseEntity.status(HttpStatus.CREATED).body(savedDevice);
         } catch (DataIntegrityViolationException e) {
-            throw new DeviceAddingException( Objects.requireNonNull(e.getRootCause()).getMessage());
+            throw new DeviceAddingException(Objects.requireNonNull(e.getRootCause()).getMessage());
         } catch (Exception e) {
-            throw new DeviceAddingException(HttpStatus.INTERNAL_SERVER_ERROR + " "  + e.getMessage());
+            throw new DeviceAddingException(HttpStatus.INTERNAL_SERVER_ERROR + " " + e.getMessage());
         }
     }
 
     @PutMapping("/updateDevice/{id}")
     public Device updateDevice(@PathVariable Long id,
-                                         @RequestBody Device updatedDevice) {
-        return deviceService.updateDevice(id,updatedDevice).get();
+                               @RequestBody Device updatedDevice) {
+        return deviceService.updateDevice(id, updatedDevice).get();
     }
 
     @GetMapping("/fetchSingleDevice/{id}")
     public ResponseEntity<Device> fetchSingleDevice(@PathVariable Long id) {
         Device device = deviceService.getSingleDevice(id)
-                .orElseThrow(() -> new DeviceNotFoundException("id = "+id));
+                .orElseThrow(() -> new DeviceNotFoundException("id = " + id));
         return ResponseEntity.ok(device);
     }
 
@@ -52,22 +52,21 @@ public class HomeController {
     }
 
     @GetMapping("/fetchByBrand")
-    public ResponseEntity<List<Device>>fetchDeviceByBrand(@RequestParam  String brand) {
-       List<Device> device = deviceService.getDeviceByBrand(brand);
-       if(!device.isEmpty())
-        return ResponseEntity.ok(device);
-       else
-           throw new DeviceNotFoundException("brand = "+brand);
+    public ResponseEntity<List<Device>> fetchDeviceByBrand(@RequestParam String brand) {
+        List<Device> device = deviceService.getDeviceByBrand(brand);
+        if (!device.isEmpty())
+            return ResponseEntity.ok(device);
+        else
+            throw new DeviceNotFoundException("brand = " + brand);
     }
 
     @GetMapping("/fetchByState")
-    public ResponseEntity<List<Device>>  fetchDeviceByState(@RequestParam  String state) {
+    public ResponseEntity<List<Device>> fetchDeviceByState(@RequestParam String state) {
         List<Device> device = deviceService.getDeviceByState(state);
-        if(!device.isEmpty())
+        if (!device.isEmpty())
             return ResponseEntity.ok(device);
         else
-            throw new DeviceNotFoundException("state = "+state);
-
+            throw new DeviceNotFoundException("state = " + state);
 
     }
 
@@ -75,13 +74,11 @@ public class HomeController {
     @GetMapping("/getAllDevices")
     public List<Device> getAllDevices() {
         return deviceService.getAllDevicesInfo();
-
     }
 
     @DeleteMapping("/deleteOneDevice/{id}")
     public void deleteOneDeviceById(@PathVariable Long id) {
-         deviceService.deleteOneDevice(id);
-
+        deviceService.deleteOneDevice(id);
     }
 
 }
