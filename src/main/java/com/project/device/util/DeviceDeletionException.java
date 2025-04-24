b885a12a9168f56aebfcafbe6578ff4ff6e45c57
yuvadany <toyuvaraj@gmail.com>
2025-04-24 14:23:30 +0200

    DeviceDeletionException.java file removed since Response entity in HomeController handles using Response Entity for delete API

--- src/main/java/com/project/device/util/DeviceDeletionException.java
+++
@@ -1,7 +0,0 @@
-package com.project.device.util;
-
-public class DeviceDeletionException extends RuntimeException {
-    public DeviceDeletionException(String parameter) {
-        super("Device can not be deleted since the device id " + parameter +" is in USE state " );
-    }
-}
