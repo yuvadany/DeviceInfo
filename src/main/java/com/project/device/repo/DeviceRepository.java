46edd7d12df84d636eaebac5786320debedd7e87
yuvadany <toyuvaraj@gmail.com>
2025-04-24 12:30:01 +0200

    find device method name updated

--- src/main/java/com/project/device/repo/DeviceRepository.java
+++ src/main/java/com/project/device/repo/DeviceRepository.java
@@ -10,11 +10,15 @@ import java.util.List;
 @Repository
 public interface  DeviceRepository extends JpaRepository<Device, Long> {
     @Query(value = "SELECT * FROM public.devices ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
-    Device findAnyOne() ;
+    Device findAnyOneDevice() ;
 
     @Query(value = "SELECT * FROM public.devices WHERE brand = :brand ", nativeQuery = true)
     List<Device> findByBrand(String brand);
 
     @Query(value = "SELECT * FROM public.devices WHERE state = :state ", nativeQuery = true)
     List<Device> findByState(String state);
+
+    @Query(value = "delete FROM public.devices WHERE id = :id ", nativeQuery = true)
+    Long deleteDeviceNotInUse(Long id);
+
 }
