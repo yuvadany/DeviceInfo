88f41c7d49a6948b2e7f19469b6ac6e44d935700
yuvadany <toyuvaraj@gmail.com>
2025-04-24 12:36:17 +0200

    All Deviceservice functions implemented

--- src/main/java/com/project/device/serviceimpl/DeviceServiceImpl.java
+++ src/main/java/com/project/device/serviceimpl/DeviceServiceImpl.java
@@ -2,190 +2,56 @@ package com.project.device.serviceimpl;
 
 import com.project.device.model.Device;
 import com.project.device.repo.DeviceRepository;
+import com.project.device.service.DeviceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
+
 
 @Service
-public class DeviceServiceImpl implements DeviceRepository {
+public class DeviceServiceImpl implements DeviceService {
 
     @Autowired
-    private DeviceRepository deviceRepository;
+    DeviceRepository deviceRepository;
     @Override
-    public void flush() {
-
+    public Device addDevice(Device device) {
+        return deviceRepository.save(device);
     }
 
     @Override
-    public <S extends Device> S saveAndFlush(S entity) {
-        return null;
+    public Device getAnyDevice() {
+        return deviceRepository.findAnyOneDevice();
     }
 
     @Override
-    public <S extends Device> List<S> saveAllAndFlush(Iterable<S> entities) {
-        return List.of();
+    public List<Device> getDeviceByBrand(String brand) {
+        return deviceRepository.findByBrand(brand);
     }
 
     @Override
-    public void deleteAllInBatch(Iterable<Device> entities) {
-
-    }
-
-    @Override
-    public void deleteAllByIdInBatch(Iterable<Long> longs) {
-
-    }
-
-    @Override
-    public void deleteAllInBatch() {
-
-    }
-
-    @Override
-    public Device getOne(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public Device getById(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public Device getReferenceById(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public <S extends Device> Optional<S> findOne(Example<S> example) {
-        return Optional.empty();
-    }
-
-    @Override
-    public <S extends Device> List<S> findAll(Example<S> example) {
-        return List.of();
-    }
-
-    @Override
-    public <S extends Device> List<S> findAll(Example<S> example, Sort sort) {
-        return List.of();
-    }
-
-    @Override
-    public <S extends Device> Page<S> findAll(Example<S> example, Pageable pageable) {
-        return null;
-    }
-
-    @Override
-    public <S extends Device> long count(Example<S> example) {
-        return 0;
-    }
-
-    @Override
-    public <S extends Device> boolean exists(Example<S> example) {
-        return false;
-    }
-
-    @Override
-    public <S extends Device, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
-        return null;
-    }
-
-    @Override
-    public <S extends Device> S save(S entity) {
-        return deviceRepository.save(entity);
-    }
-
-    @Override
-    public <S extends Device> List<S> saveAll(Iterable<S> entities) {
-        return List.of();
-    }
-
-    @Override
-    public Optional<Device> findById(Long aLong) {
-        return deviceRepository.findById(aLong);
-    }
-
-    @Override
-    public boolean existsById(Long aLong) {
-        return false;
-    }
-
-    @Override
-    public List<Device> findAll() {
+    public List<Device> getAllDevicesInfo() {
         return deviceRepository.findAll();
     }
 
     @Override
-    public List<Device> findAllById(Iterable<Long> longs) {
-        return List.of();
-    }
-
-    @Override
-    public long count() {
-        return 0;
-    }
-
-    @Override
-    public void deleteById(Long aLong) {
-       deviceRepository.deleteById(aLong);
-    }
-
-    @Override
-    public void delete(Device entity) {
-
-    }
-
-    @Override
-    public void deleteAllById(Iterable<? extends Long> longs) {
-
-    }
-
-    @Override
-    public void deleteAll(Iterable<? extends Device> entities) {
-
-    }
-
-    @Override
-    public void deleteAll() {
-
-    }
-
-    @Override
-    public List<Device> findAll(Sort sort) {
-        return List.of();
-    }
-
-    @Override
-    public Page<Device> findAll(Pageable pageable) {
-        return null;
-    }
-
-    @Override
-    public Device findAnyOne() {
-        return deviceRepository.findAnyOne();
-    }
-
-    @Override
-    public List<Device> findByBrand(String brand) {
-        return deviceRepository.findByBrand(brand);
-    }
-
-
-    @Override
-    public List<Device> findByState(String state) {
+    public List<Device> getDeviceByState(String state) {
         return deviceRepository.findByState(state);
     }
 
+    @Override
+    public Optional<Device> getSingleDevice(Long id) {
+        return deviceRepository.findById(id);
+    }
+
+    @Override
+    public Long deleteOneDevice(Long id) {
+        deviceRepository.deleteById(id);
+        return id;
+    }
+
+    @Override
     public Optional<Device> updateDevice(Long id, Device updatedDevice) {
         return deviceRepository.findById(id)
                 .map(existingDevice -> {
@@ -196,4 +62,5 @@ public class DeviceServiceImpl implements DeviceRepository {
                     return existingDevice;
                 });
     }
+
 }
