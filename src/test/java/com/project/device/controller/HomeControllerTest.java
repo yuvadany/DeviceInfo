715f5fec25fccd3208282ce9d15163bf27589d63
yuvadany <toyuvaraj@gmail.com>
2025-04-25 11:02:52 +0200

    Testcontainer added  for POstgresql INtegration Test

--- src/test/java/com/project/device/controller/HomeControllerTest.java
+++ src/test/java/com/project/device/controller/HomeControllerTest.java
@@ -8,7 +8,11 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDateTime;
@@ -27,15 +31,31 @@ public class HomeControllerTest {
 
     @Autowired
     private DeviceRepository deviceRepository;
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17")
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpass");
 
-   @BeforeEach
+
+    @DynamicPropertySource
+    static void registerPgProperties(DynamicPropertyRegistry registry) {
+        postgres.start();
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+
+    @BeforeEach
     void setup() {
         deviceRepository.deleteAll();
     }
 
+
     @Test
     void shouldFetchSingleDevice() throws Exception {
-        Device device = new Device(null, "Router", "Cisco", Device.State.AVAILABLE, LocalDateTime.now(), null);
+        Device device = new Device(null, "Router", "CISCO", Device.State.AVAILABLE, LocalDateTime.now(), null);
 
         Device savedDevice = deviceRepository.save(device);
 
@@ -43,14 +63,14 @@ public class HomeControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(savedDevice.getId()))
                 .andExpect(jsonPath("$.name").value("Router"))
-                .andExpect(jsonPath("$.brand").value("Cisco"))
+                .andExpect(jsonPath("$.brand").value("CISCO"))
                 .andExpect(jsonPath("$.state").value("AVAILABLE"))
                 .andExpect(jsonPath("$.creationTime").exists());
     }
 
     @Test
     void shouldReturnNotFoundIfDeviceMissing() throws Exception {
-        mockMvc.perform(get("/fetchSingleDevice/9999"))
+        mockMvc.perform(get("/v1/devices/fetchSingleDevice/9999"))
                 .andExpect(status().isNotFound());
     }
 }
